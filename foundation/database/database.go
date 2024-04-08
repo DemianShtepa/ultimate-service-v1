e74feaef0d63ad5d8f75e1b62f5070adb7378045
Demian <demyan.shtepa@gmail.com>
2024-04-08 16:06:04 +0300

    Add database

---
+++ foundation/database/database.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"net/url"
+)
+
+type Config struct {
+	Host     string
+	User     string
+	Password string
+	Database string
+}
+
+func Open(config Config) (*sqlx.DB, error) {
+	query := make(url.Values)
+	query.Set("timezone", "utc")
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword(config.User, config.Password),
+		Host:     config.Host,
+		Path:     config.Database,
+		RawQuery: query.Encode(),
+	}
+
+	return sqlx.Open("postgres", u.String())
+}
+
+func StatusCheck(ctx context.Context, db *sqlx.DB) error {
+	var tmp bool
+
+	return db.QueryRowxContext(ctx, "SELECT true").Scan(&tmp)
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-playground/validator/v10 v10.19.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/pkg/errors v0.9.1
 )
 
